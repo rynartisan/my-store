19512dbafa6cc32cfffc72f2aad0673fafd59754
rynartisan <rayantaw@gmail.com>
2023-07-03 18:50:40 +0300

    Initial commit. I added the main components and copied the provided CSS classes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ConfirmationComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
