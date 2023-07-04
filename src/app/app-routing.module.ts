c0f39abb4a64597f9203f13367ac1867d9e5c1d1
Rayan Altamimi <raaltamimi@tahakom.com>
2023-07-04 10:13:27 +0300

    Set up the navbar and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {ProductItemComponent} from "./product-item/product-item.component";
+import {CartComponent} from "./cart/cart.component";
+import {ConfirmationComponent} from "./confirmation/confirmation.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductItemComponent},
+  {path: 'item-detail', component: ProductItemComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'check-out', component: ConfirmationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
