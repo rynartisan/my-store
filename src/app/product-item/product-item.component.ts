5b3755a0f000e5d28c723f9b7949ae11b332deb0
Rayan Altamimi <raaltamimi@tahakom.com>
2023-07-05 12:29:00 +0300

    isolate component item code

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Product} from "../model/product";
 
 @Component({
   selector: 'app-product-item',
@@ -6,5 +7,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent {
-
+  @Input() product: Product = {description: "", id: 0, name: "", price: 0, url: ""};
 }
