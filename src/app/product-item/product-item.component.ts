f0ba854705d8184d60a397cce53748f585eb2418
Rayan Altamimi <raaltamimi@tahakom.com>
2023-07-06 13:25:21 +0300

    Button improvements.

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,8 +1,9 @@
 import {Component, Input} from '@angular/core';
 import {Product} from "../model/product";
-import {faShoppingCart} from "@fortawesome/free-solid-svg-icons";
+import {faCheckCircle, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 import {CartService} from "../service/cart.service";
 import {CartProduct} from "../model/cart.product";
+import {IconDefinition} from "@fortawesome/free-regular-svg-icons";
 
 @Component({
   selector: 'app-product-item',
@@ -13,8 +14,8 @@ export class ProductItemComponent {
   @Input() product: Product = {description: "", id: 0, name: "", price: 0, url: ""};
   quantity: number = 1;
   quantities: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  protected readonly faShoppingCart = faShoppingCart;
-  protected readonly parseInt = parseInt;
+  buttonLabel: string = 'Add to Cart';
+  buttonIcon: IconDefinition = faShoppingCart;
 
   constructor(private cartService: CartService) {
   }
@@ -26,11 +27,18 @@ export class ProductItemComponent {
     this.cartService.addCartProduct(cartProduct);
     // Reset the quantity to original.
     this.quantity = 1;
+    // Change the button label.
+    this.buttonLabel = 'Added to Cart!';
+    // Change the icon
+    this.buttonIcon = faCheckCircle;
   }
 
-  // I had to add this since for some reason NgModel treats input fields as string by default and concatenates numbers instead of adding them.
-  // More on the problem: https://stackoverflow.com/questions/37921596/option-tag-returning-string-value-instead-of-number-for-ngmodel-in-angular
-  onChangeSelection($event: any) {
+  /**
+   * I had to add this since for some reason NgModel treats input fields as string by default and concatenates numbers instead of adding them.
+   * More on the problem: https://stackoverflow.com/questions/37921596/option-tag-returning-string-value-instead-of-number-for-ngmodel-in-angular
+   * @param $event
+   */
+  onChangeSelection($event: any): void {
     this.quantity = parseInt($event);
   }
 }
