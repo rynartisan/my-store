56857192c17a3acfe28490e8ede0574298dbfe5c
Rayan Altamimi <raaltamimi@tahakom.com>
2023-07-06 14:06:35 +0300

    Initial template for cart component.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CartProduct} from "../model/cart.product";
+import {CartService} from "../service/cart.service";
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  orderProducts: CartProduct[] = [];
 
+  constructor(private cartService: CartService) {
+  }
+
+  ngOnInit(): void {
+    this.initializeCartProducts();
+  }
+
+  private initializeCartProducts() {
+    this
+      .cartService
+      .getCartProducts()
+      .subscribe(value => this.orderProducts = value);
+  }
 }
