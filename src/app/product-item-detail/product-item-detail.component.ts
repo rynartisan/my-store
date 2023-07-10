8225c658e0ed7efa5be1ef3c3dc79c2e8b7a3b27
Rayan Altamimi <raaltamimi@tahakom.com>
2023-07-10 12:12:49 +0300

    Added "other products you might like".

--- src/app/product-item-detail/product-item-detail.component.ts
+++ src/app/product-item-detail/product-item-detail.component.ts
@@ -3,6 +3,8 @@ import {Product} from "../model/product";
 import {ActivatedRoute} from "@angular/router";
 import {faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 import {IconDefinition} from "@fortawesome/free-regular-svg-icons";
+import {ProductService} from "../service/product.service";
+import {shuffle} from "lodash"
 
 @Component({
   selector: 'app-product-item-detail',
@@ -11,13 +13,14 @@ import {IconDefinition} from "@fortawesome/free-regular-svg-icons";
 })
 export class ProductItemDetailComponent implements OnInit {
   @Input() id!: number;
-  product?: Product;
+  product: Product = {description: "", id: 0, name: "", price: 0, url: ""};
   buttonIcon: IconDefinition = faShoppingCart;
   buttonLabel: string = 'Add to cart';
   quantity: number = 1;
   quantities: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  private products: Product[] = [];
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, private productService: ProductService) {
   }
 
   addToCart() {
@@ -29,6 +32,21 @@ export class ProductItemDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.initializeMainProduct();
+    this
+      .productService
+      .getProducts()
+      .subscribe(value => {
+        this.products = value
+      })
+  }
+
+  getRandomProducts(id: number): Product[] {
+    return shuffle(this.products).filter(value => value.id !== id).slice(0, 4);
+  }
+
+  private initializeMainProduct() {
+    console.log(`id -> ${this.id}`)
     this
       .activatedRoute
       .data
