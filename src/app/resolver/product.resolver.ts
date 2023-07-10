805b6be075eb76651393c262aa52b0fe5f0dabea
Rayan Altamimi <raaltamimi@tahakom.com>
2023-07-10 14:42:36 +0300

    Enabled noImplicitAny

--- src/app/resolver/product.resolver.ts
+++ src/app/resolver/product.resolver.ts
@@ -2,10 +2,11 @@ import { ActivatedRouteSnapshot, ResolveFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { ProductService } from '../service/product.service';
 import { Product } from '../model/product';
+import { Observable } from 'rxjs';
 
 export const productResolver: ResolveFn<Product> = (
   route: ActivatedRouteSnapshot,
-) => {
+): Observable<Product> => {
   const id = route.paramMap.get('id')!;
   const productService = inject(ProductService);
   return productService.findProduct(parseInt(id));
